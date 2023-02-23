ebfc922999207d0368aabfb7b09b1bbe63669da0
AgentAPM <54836547+AgentAPM@users.noreply.github.com>
2023-02-23 20:35:23 +0100

    Zadanie domowe - dzień 6

---
+++ ChallengeApp/Employee.cs
@@ -0,0 +1,32 @@
+﻿namespace ChallengeApp
+{
+
+    class Employee
+    {
+        private string name;
+        private string lastName;
+        private int age;
+        private List<int> grades;
+        public Employee(string name, string lastName, int age)
+        {
+            this.name = name;
+            this.lastName = lastName;
+            this.age = age;
+            this.grades = new List<int>();
+        }
+
+        public void AssignGrade(int grade)
+        {
+            grades.Add(grade);
+        }
+        public int GetScore()
+        {
+            return grades.Sum();
+        }
+        public string GetPersonals()
+        {
+            string summary = $"{this.name} {this.lastName}, lat {this.age}";
+            return summary;
+        }
+    }
+}
